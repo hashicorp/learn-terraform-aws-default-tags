e920fda978ece9a9a542cb6d26b0a5b938e31f08
ritsok <msokolova13@gmail.com>
2021-07-20 15:11:30 -0400

    Add ASG output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "instance_id" {
   description = "EC2 instance ID"
   value       = aws_instance.example.id
 }
+
+output "asg_id" {
+  description = "Auto Scaling group ID"
+  value       = aws_autoscaling_group.example.id
+}
