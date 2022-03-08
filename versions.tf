4cce2f85bb6d02a37758dfd25968ecfb62a57474
ritsok <msokolova13@gmail.com>
2022-03-08 12:59:58 -0500

    Bump versions, add default tags

--- versions.tf
+++ versions.tf
@@ -1,10 +1,11 @@
 terraform {
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.38"
+      version = "~> 4.4.0"
     }
   }
 
-  required_version = ">= 0.15"
+  required_version = ">= 1.1"
 }
